b371d3d2a6fd1067f522ed75dec0ae26bcdd6926
Marviel <lukebechtel4@gmail.com>
2024-11-23 09:32:25 -0800

    rm comment

--- apps/web/app/api/genObject/route.api.ts
+++ apps/web/app/api/genObject/route.api.ts
@@ -9,8 +9,6 @@ export async function POST(request: Request) {
     try {
         const body = await request.json();
 
-        console.log(body);
-
         const { schema: schemaPreParsed, prompt, ...rest } = body;
 
         const schema = unsafeJsonSchemaToZod(schemaPreParsed);
